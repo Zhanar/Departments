8f07bc2d3fb03c87cd8876c560a4917a16442316
Zhanar <zh.yegizbayeva@gmail.com>
2016-05-20 01:43:02 +0600

    six commit

---
+++ app/src/main/java/com/assignment/departments/ChangeTopManagerActivity.java
@@ -0,0 +1,82 @@
+package com.assignment.departments;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.assignment.departments.Model.Department;
+import com.assignment.departments.Model.Employee;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ChangeTopManagerActivity extends AppCompatActivity {
+
+    TextView textView;
+    ArrayList<Employee> listTopManagers;
+    ListView listView;
+    EmployeeActivity.ListAdapter listAdapter;
+    Department department;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_top_manager);
+
+        department = (Department)getIntent().getSerializableExtra("department");
+        listView = (ListView)findViewById(R.id.listViewChangeTopManager);
+        textView = (TextView)findViewById(R.id.textViewTopManager);
+        if (department == null){
+            textView.setText("There are no top managers in that department.");
+        }
+        else {
+            textView.setText("Choose top manager:");
+            listTopManagers = new ArrayList<>();
+            for(int i = 0; i < department.getEmployees().size(); i++) {
+                listTopManagers.add(department.getEmployees().get(i));
+            }
+            listAdapter = new EmployeeActivity.ListAdapter(this, 0, listTopManagers);
+            listView.setAdapter(listAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    AsyncHttpClient httpClient = new AsyncHttpClient();
+                    RequestParams params = new RequestParams();
+
+                    params.put("userId", listTopManagers.get(position).getId());
+                    params.put("orgUnitId", department.getId());
+
+                    httpClient.post("http://orgunitapi.azurewebsites.net/orgunit/AddHeader", params, new JsonHttpResponseHandler() {
+
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            Log.d("HTTP", "added-result " + response);
+                           // head.setText(headName);
+                            Intent intent = new Intent(ChangeTopManagerActivity.this, SubDepartmentActivity.class);
+                            intent.putExtra("department", department);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                            super.onFailure(statusCode, headers, responseString, throwable);
+                            Log.d("Fail HTTP", "" + responseString);
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/assignment/departments/SubDepartmentActivity.java
+++ app/src/main/java/com/assignment/departments/SubDepartmentActivity.java
@@ -1,20 +1,14 @@
 package com.assignment.departments;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.ContextMenu;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -22,18 +16,14 @@ import android.widget.TextView;
 
 import com.assignment.departments.Model.Department;
 import com.assignment.departments.Model.Employee;
-import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -44,7 +34,7 @@ public class SubDepartmentActivity extends AppCompatActivity {
     TextView textViewHead;
     ArrayList<Department> listSubDepartment;
     DepartmentActivity.ListAdapter listAdapter;
-    Department d;
+    Department department;
     AsyncHttpClient httpClient;
     Button changeHead;
 
@@ -53,23 +43,24 @@ public class SubDepartmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_department);
 
-        d = (Department)getIntent().getSerializableExtra("department");
+        department = (Department)getIntent().getSerializableExtra("department");
 
-        getSupportActionBar().setTitle(d.getTitle());
+        getSupportActionBar().setTitle(department.getTitle());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        changeHead = (Button)findViewById(R.id.buttonChangeHead);
         textViewHead = (TextView)findViewById(R.id.textViewHead);
-        textViewHead.setText("Head of Department: " + d.getHeadUser().getLogin());
+        textViewHead.setText("Head of Department: " + department.getHeadUser().getLogin());
 
         textView = (TextView)findViewById(R.id.textViewDepartments);
-        //textView.setText(d.getTitle());
+        //textView.setText(department.getTitle());
         listSubDepartment = new ArrayList<>();
         listView = (ListView)findViewById(R.id.listViewSubDepartment);
 
-        if (d.getOrgUnitChilds().size() != 0) {
+        if (department.getOrgUnitChilds().size() != 0) {
             textView.setText("Sub Departments:");
-            for(int i = 0; i < d.getOrgUnitChilds().size(); i++) {
-                listSubDepartment.add(d.getOrgUnitChilds().get(i));
+            for(int i = 0; i < department.getOrgUnitChilds().size(); i++) {
+                listSubDepartment.add(department.getOrgUnitChilds().get(i));
             }
         }
         else {
@@ -81,9 +72,9 @@ public class SubDepartmentActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(SubDepartmentActivity.this, EmployeeActivity.class);
-                i.putExtra("subDepartment", listSubDepartment.get(position));
-                startActivity(i);
+                Intent intent = new Intent(SubDepartmentActivity.this, EmployeeActivity.class);
+                intent.putExtra("subDepartment", listSubDepartment.get(position));
+                startActivity(intent);
             }
         });
 
@@ -128,7 +119,7 @@ public class SubDepartmentActivity extends AppCompatActivity {
         AsyncHttpClient httpClient = new AsyncHttpClient();
         RequestParams params = new RequestParams();
         params.put("title", editText.getText().toString());
-        params.put("parrentOrgUnitId", d.getId());
+        params.put("parrentOrgUnitId", department.getId());
         httpClient.post("http://orgunitapi.azurewebsites.net/OrgUnit/Create", params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
@@ -139,11 +130,9 @@ public class SubDepartmentActivity extends AppCompatActivity {
 
     public void changeHead(View view) {
 
-        changeHead = (Button)findViewById(R.id.buttonChangeHead);
 
-        ArrayList<Employee> head = new ArrayList<>();
-        for(int i = 0; i < d.getEmployees().size(); i++) {
-            head.add(d.getEmployees().get(i));
-        }
+        Intent intent = new Intent(SubDepartmentActivity.this, ChangeTopManagerActivity.class);
+        intent.putExtra("department", department);
+        startActivity(intent);
     }
 }
